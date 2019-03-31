a472febd4e7e2294a675977688cf79331f52011c
Stef Claus <stef.claus@gmail.com>
2019-03-31 19:43:38 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,57 @@
 class FiguresController < ApplicationController
-  # add controller methods
+
+  get '/figures' do
+   @figures = Figure.all
+   erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    erb :'figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+#   @pets = Pet.all
+    erb :'/figures/edit'
+  end
+
+  post '/figures' do
+    @figures = Figure.create(:name => params["figure"])
+      if !params["title"]["name"].empty?
+        @figure.title = Title.create(name: params["title"]["name"])
+        #if !params["landmark"]["name"].empty? && !params["landmark"]["year_completed"].empty?
+        #  @figure.landmark.name = Landmark.create(name: params["landmark"]["name"])
+        #  @figure.landmark.year_completed = Landmark.create(year_completed: params["landmark"]["year_completed"])
+        #end
+      #end
+    end
+
+    #@figure.title = Title.find_or_create_by(:name => params["Title Name"])
+   #@figure.landmark = params[:landmark]
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
+ patch '/figures/:id' do
+   @figure = Figure.find(params[:id])
+
+   ####### the following bug fix is required so that it's possible to remove ALL previous pets from owner.
+  # if !params[:owner].keys.include?("pet_ids")
+   #params[:owner]["pet_ids"] = []
+   #end
+   ####### End of fix
+
+  # @owner.update(params["owner"])
+   #if !params["pet"]["name"].empty?
+    # @owner.pets << Pet.create(name: params["pet"]["name"])
+   #end
+   redirect "figures/#{@figure.id}"
+ end
+
+
 end
