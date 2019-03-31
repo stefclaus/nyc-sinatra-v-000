2103bfd7e74f1e2c6e4fc1e41eddea99a6699274
Stef Claus <stef.claus@gmail.com>
2019-03-31 17:10:03 +0000

    Done.

--- db/migrate/04_create_title_figures.rb
+++
@@ -1,8 +0,0 @@
-class CreateTitleFigures < ActiveRecord::Migration[5.2]
-  def change
-    create_table :title_figures do |t|
-      t.integer :title_id
-      t.integer :figure_id
-    end
-  end
-end
