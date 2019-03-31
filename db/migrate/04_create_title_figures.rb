d3ef86389f47e92d1bf4ed421328c12270ff9319
Stef Claus <stef.claus@gmail.com>
2019-03-31 16:16:44 +0000

    Done.

--- db/migrate/04_create_title_figures.rb
+++ db/migrate/04_create_title_figures.rb
@@ -2,7 +2,7 @@ class CreateTitleFigures < ActiveRecord::Migration[5.2]
   def change
     create_table :title_figures do |t|
       t.integer :title_id
-      t.integer :song_id
+      t.integer :figure_id
     end
   end
 end
