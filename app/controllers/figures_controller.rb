8b94216b4ca4a2f79fbccc60ff7550ec7ddad240
Stef Claus <stef.claus@gmail.com>
2019-04-01 21:26:19 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -21,35 +21,34 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-    @figures = Figure.create(:name => params["figure"])
-      if !params["title"]["name"].empty?
-        @figure.title = Title.create(name: params["title"]["name"])
-        #if !params["landmark"]["name"].empty? && !params["landmark"]["year_completed"].empty?
-        #  @figure.landmark.name = Landmark.create(name: params["landmark"]["name"])
-        #  @figure.landmark.year_completed = Landmark.create(year_completed: params["landmark"]["year_completed"])
-        #end
-      #end
+    @figure = Figure.create(params["figure"])
+
+    unless params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
     end
 
-    #@figure.title = Title.find_or_create_by(:name => params["Title Name"])
-   #@figure.landmark = params[:landmark]
+    unless params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
     @figure.save
     redirect to "/figures/#{@figure.id}"
   end
 
+
  patch '/figures/:id' do
    @figure = Figure.find(params[:id])
+   @figure.update(params[:figure])
 
-   ####### the following bug fix is required so that it's possible to remove ALL previous pets from owner.
-  # if !params[:owner].keys.include?("pet_ids")
-   #params[:owner]["pet_ids"] = []
-   #end
-   ####### End of fix
+   unless params[:title][:name].empty?
+     @figure.titles << Title.create(params[:title])
+   end
 
-  # @owner.update(params["owner"])
-   #if !params["pet"]["name"].empty?
-    # @owner.pets << Pet.create(name: params["pet"]["name"])
-   #end
+   unless params[:landmark][:name].empty?
+     @figure.landmarks << Landmark.create(params[:landmark])
+   end
+
+   @figure.save
    redirect "figures/#{@figure.id}"
  end
 
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,55 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  get '/landmarks' do
+   @landmarks = Landmark.all
+   erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/edit'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params["landmark"])
+
+    unless params[:title][:name].empty?
+      @landmark.titles << Title.create(params[:title])
+    end
+
+    unless params[:figure][:name].empty?
+      @landmark.figure << Landmark.create(params[:figure])
+    end
+
+    @landmark.save
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
+
+ patch '/landmarks/:id' do
+   @landmark = Landmark.find(params[:id])
+   @landmark.update(params[:landmark])
+
+  # unless params[:title][:name].empty?
+  #   @landmark.titles << Title.create(params[:title])
+   #end
+
+  # unless params[:figure][:name].empty?
+#     @landmark.figure << Landmark.create(params[:figure])
+#   end
+
+   @landmark.save
+   redirect to "/landmarks/#{@landmark.id}"
+ end
+
+
 end
